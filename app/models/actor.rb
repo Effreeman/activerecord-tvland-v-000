b906d7e0309d6cbe39b06d51a1098af7c99a9f78
Emma Freeman <freemoe04@gmail.com>
2018-06-27 16:39:51 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,15 +1,8 @@
-  
-  class Actor < ActiveRecord::Base
-    has_many :characters
-    has_many :shows, through: :characters
+class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
 
-    def full_name
-      self.first_name + " " + self.last_name
-    end
-
-    def list_roles
-      self.characters.map do |c|
-        c.name + " - " + c.show.name
-      end
-    end
+  def full_name
+    puts "#{self.first_name} #{self.last_name}"
   end
+end
