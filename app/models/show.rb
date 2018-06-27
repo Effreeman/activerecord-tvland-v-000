07140c6641320f13fe965fabf14f1026cccb9a11
Emma Freeman <freemoe04@gmail.com>
2018-06-27 16:34:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,8 @@
+
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
-  belongs_to :network
+  belongs_to:network
 
   def build_network(letters)
     self.network = Network.find_or_create_by(letters)
