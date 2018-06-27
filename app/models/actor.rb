17cf5bea0cf812f5c229efc2019d04f6e49dfc01
Emma Freeman <freemoe04@gmail.com>
2018-06-27 16:44:14 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,9 +1,14 @@
+class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
 
-class Character < ActiveRecord::Base
-  belongs_to :show
-  belongs_to :actor
+  def full_name
+     "#{self.first_name} #{self.last_name}"
+  end
 
-  def say_that_thing_you_say
-    "#{self.name} always says: #{self.catchphrase}"
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 end
