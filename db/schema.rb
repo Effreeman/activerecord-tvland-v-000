9d3e7e0aad926cd5d0a6f93df68ecf2c47d4747e
Emma Freeman <freemoe04@gmail.com>
2018-06-27 16:43:11 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -20,8 +21,8 @@ ActiveRecord::Schema.define(version: 9) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
-    t.string "catchphrase"
     t.integer "actor_id"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -33,6 +34,8 @@ ActiveRecord::Schema.define(version: 9) do
     t.string "name"
     t.integer "network_id"
     t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
