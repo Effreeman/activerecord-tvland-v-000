c0cb4ec7f59e2ad6a1f5d20e6bae83971496c417
Emma Freeman <freemoe04@gmail.com>
2018-06-27 16:22:51 +0000

    automatically backed up by learn

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -4,8 +4,6 @@ describe Show do
   let(:show) {Show.new}
 
   it "has data attributes" do
-    #TODO: You'll need to create a new migration to add
-    #the necessary columns to the shows table
     community = Show.create(:name => "Community", :day => "Thursday", :season => "Winter")
     expect(community.day).to eq("Thursday")
     expect(community.season).to eq("Winter")
